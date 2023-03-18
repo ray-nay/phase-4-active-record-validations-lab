6552ad558cbd46ba631ff546b2ea86c6097f762a
Renee Mumbi <1723renee@gmail.com>
2023-03-18 23:04:28 +0300

    Done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+     # All authors have a name
+  validates :name, presence: true
+
+  # No two authors have the same name
+  validates :name, uniqueness: true
+
+  # Author phone numbers are exactly ten digits
+  validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+#title can not be empty
+    validates :title, presence: true
+  # Post content is at least 250 characters long
+  validates :content, length: { minimum: 250 }
+
+  # Post summary is a maximum of 250 characters
+  validates :summary, length: { maximum: 250 }
+
+  # Post category is either Fiction or Non-Fiction
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+  validate :clickbait_title
+
+  private
+
+  def clickbait_title
+    unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess"))
+      errors.add(:title, 'must be clickbait-y')
+    end
+  end
+
 end
